27cc05703afb68f6613e6850e22d8a4c0a1cbc3f
lei.guo <lei.guo@hootsuite.com>
2022-11-05 23:54:40 -0700

    implement the path expansion

--- journey.js
+++ journey.js
@@ -30,7 +30,8 @@ d3.xml(svgURL).then((data) => {
   const ec2 = svg.selectAll("[id^=ec2]").nodes();
   const servicePods = d3.select("#servicePods").node();
   const servicePodsPos = getElementBBox(servicePods);
-  console.log(servicePods);
+  const workerNode = d3.select("[id=clust8]").select("path");
+  const workerNodePos = getElementBBox(workerNode.node());
 
   const ec2Migration = (i) => {
     d3.select(ec2[i])
@@ -50,8 +51,24 @@ d3.xml(svgURL).then((data) => {
             "https://raw.githubusercontent.com/mingrammer/diagrams/834899659ae2e4f9f0d0dd9d01a4d7f31513d726/resources/k8s/compute/pod.png"
           );
 
-        d3.select("[id=clust8]").size([400, 300]);
         d3.select("[id=clust8]").node().appendChild(ec2[i]);
+
+        const path = d3.path();
+        path.moveTo(workerNodePos.x, workerNodePos.y);
+        path.lineTo(
+          workerNodePos.x + workerNodePos.w * (i + 1),
+          workerNodePos.y
+        );
+        path.lineTo(
+          workerNodePos.x + workerNodePos.w * (i + 1),
+          workerNodePos.y + workerNodePos.h
+        );
+        path.lineTo(workerNodePos.x, workerNodePos.y + workerNodePos.h);
+        path.closePath();
+        console.log(path);
+
+        workerNode.attr("d", path);
+
         if (i < ec2.length - 1) {
           ec2Migration(i + 1);
         }
