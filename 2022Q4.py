dec82c2fcaed1de9ae654ff30665f8e0d1c94de3
lego <lleiguo@gmail.com>
2023-04-08 20:50:51 -0700

    added 2023 Q1 changes

--- 2022Q4.py
+++ 2022Q4.py
@@ -2,10 +2,9 @@
 from xml.sax.xmlreader import AttributesImpl
 from diagrams import Diagram, Cluster, Edge
 
-from diagrams.aws.compute import EC2, AutoScaling, EC2ElasticIpAddress
-from diagrams.aws.database import RDS, Aurora, ElasticacheForRedis
-from diagrams.aws.network import Route53, ALB, NLB, CloudFront
-from diagrams.aws.storage import S3
+from diagrams.aws.compute import EC2, AutoScaling
+from diagrams.aws.database import Aurora, Dynamodb
+from diagrams.aws.network import NLB
 
 from diagrams.k8s.compute import Pod
 from diagrams.onprem.queue import Kafka 
@@ -13,54 +12,56 @@ from diagrams.onprem.network import Zookeeper
 
 
 graph_attr = {
-    "fontsize": "250",
+    "fontsize": "50",
     "bgcolor": "transparent"
 }
 
-with Diagram(filename="2022Q4", show=False, direction="TB", graph_attr=graph_attr, outformat=["png"]):
+with Diagram("2022 Q4", filename="2022Q4", show=False, direction="TB", graph_attr=graph_attr, outformat=["png"]):
 
-    with Cluster(label="Production/Staging", direction="TB", graph_attr={"id": "cluster_production"}):
+    with Cluster(label="Production/Staging", direction="TB", graph_attr={"id": "cluster_production","bgcolor": "white", "fontsize": "20"}):
 
-        with Cluster("Aperture", graph_attr={"id": "cluster_aperture"}):
+        with Cluster("Aperture", graph_attr={"id": "cluster_aperture", "fontsize": "20"}):
             traefik = AutoScaling("Traefik Aperture",
                                 id="ec2_aperture_traefik")
-            authFacade = AutoScaling("Auth Facade", id="ec2_aperture_authfacade")
 
-        with Cluster("EC2 Services", graph_attr={"id": "cluster_ec2"}):
-                EC2Services = [EC2("Auth Facade", id="ec2-AuthFacade"),
-                            EC2("Webhook Egress Proxy", id="ec2-WebhookEgressProxy"),
-                            EC2("External API", id="ec2-ExternalAPI"),
-                            EC2("Data Deletion", id="ec2-DataDeletion")]
-                EC2Other = EC2("Other EC2 Services", id="ec2-other")
+        with Cluster("EC2 Services", graph_attr={"id": "cluster_ec2","fontsize": "20"}):
+            authfacade = EC2("Auth Facade", id="ec2-AuthFacade")
+            EC2Services = [EC2("Webhook Egress Proxy", id="ec2-WebhookEgressProxy"),
+                        EC2("External API", id="ec2-ExternalAPI")]
 
-        with Cluster("Vault"):
+        with Cluster("Vault", graph_attr={"id": "cluster_vault", "fontsize": "20"}):
             vault = EC2("Vault", id="ec2-vault")
 
-        with Cluster("Event Bus (Kafka)", direction="LR", graph_attr={"id": "cluster_kafka"}):
+        with Cluster("Event Bus (Kafka)", direction="LR", graph_attr={"id": "cluster_kafka", "fontsize": "20"}):
             kafka = Kafka("Kafka")
             zookeeper = Zookeeper("Zookeeper")
 
-        with Cluster("Stateful Services", graph_attr={"id": "cluster_data_storage"}):
-            rds = RDS("Aurora 5.6")
-            cache = ElasticacheForRedis("Redis")
-            datastore = [rds, cache]
+        with Cluster("Stateful Services", graph_attr={"id": "cluster_data_storage","fontsize": "20"}):
+            rds = Aurora("Aurora 5.6")
+            dynamo = Dynamodb("DynamoDB")
+            datastore = [rds, dynamo]
 
-    with Cluster("Kubernetes Cluster", direction="TB", graph_attr={"id": "cluster_k8s"}):
-            k8sIngress = NLB("Traefik Ingress")
-            servicePods = [Pod("Dashboard PHP", id="dashboard"), Pod("Amplify", id="amplify")]
-            servicePod = Pod("Other Microservices Pods... ", id="servicePods")
-
-    with Cluster("US-West-1 - Owly", graph_attr={"id": "cluster_owly"}):
+    with Cluster("US-West-1 - Owly", graph_attr={"id": "cluster_owly","bgcolor": "white", "fontsize": "20"}):
         haproxy = NLB("HAProxy", id="haproxy")
         owlyweb = EC2("Owly Web", id="owlyweb")
-        owlydb = EC2("Owly Analytics", id="owly-analytics")
+        owlydb = EC2("Owly DB", id="owly-analytics")
+        owlyAnalytics = EC2("Owly Analytics DB", id="owly-analytics")
+        owly = [owlyAnalytics, owlydb]
+
+    with Cluster("Kubernetes Cluster", direction="TB", graph_attr={"id": "cluster_k8s","fontsize": "20"}):
+            k8sIngress = AutoScaling("Traefik K8s Ingress")
+            servicePods = [Pod("Dashboard PHP", id="dashboard"), Pod("Amplify", id="amplify")]
+            servicePod = Pod("200+ Microservices", id="servicePods")
+
 
 # Path:
     traefik >> k8sIngress
-    authFacade >> k8sIngress
     traefik >> Edge(reverse=True, id="edge_ec2_traefik") >> EC2Services
     servicePod >> Edge(reverse=True) << kafka
     vault >> Edge(reverse=True) << servicePods
-    k8sIngress >> Edge(reverse=True) << servicePod, servicePods
+    k8sIngress >> Edge(reverse=True) << servicePods
+    k8sIngress >> Edge(reverse=True) << servicePod
     servicePod >> Edge(reverse=True, id="edge_datastore") << datastore
-    EC2Other >> Edge(reverse=True, id="edge_ec2other") << datastore 
+    authfacade >> Edge(reverse=True, id="edge_ec2other") << datastore 
+    authfacade >> Edge(reverse=True, id="edge_ec2other") << kafka 
+    haproxy >> Edge(reverse=True, id="edge_kafka") << owlyweb >> owly
--- 2023Q1.py
+++ 2023Q1.py
@@ -2,54 +2,48 @@
 from xml.sax.xmlreader import AttributesImpl
 from diagrams import Diagram, Cluster, Edge
 
-from diagrams.aws.compute import EC2, AutoScaling, ECS, EC2ElasticIpAddress
-from diagrams.aws.database import RDS, Aurora, ElasticacheForRedis, ElasticacheForMemcached
-from diagrams.aws.network import Route53, ALB, NLB, CloudFront
-from diagrams.aws.storage import S3
+from diagrams.aws.compute import ECS
+from diagrams.aws.database import Aurora, Dynamodb
 from diagrams.aws.analytics import ManagedStreamingForKafka
 
 
 from diagrams.k8s.compute import Pod
-from diagrams.onprem.queue import Kafka 
-from diagrams.onprem.network import Zookeeper
-
 
 graph_attr = {
     "fontsize": "60",
     "bgcolor": "transparent"
 }
 
-with Diagram(filename="2023Q1", show=False, direction="TB", graph_attr=graph_attr, outformat=["png"]):
+with Diagram("2023 Q1", filename="2023Q1", show=False, direction="TB", graph_attr=graph_attr, outformat=["png"]):
 
-    with Cluster(label="Production/Staging", direction="TB", graph_attr={"id": "cluster_production"}):
 
-        with Cluster("Stateful Services", graph_attr={"id": "cluster_data_storage"}):
-            rds = RDS("Aurora 5.7")
-            cache = ElasticacheForRedis("Redis")
-            msk = ManagedStreamingForKafka("MSK")
-            datastore = [rds, cache]
-
-        with Cluster("EC2 Services", graph_attr={"id": "cluster_ec2"}):
-                EC2Services = [EC2("External API", id="ec2-ExternalAPI"),
-                            EC2("Data Deletion", id="ec2-DataDeletion")]
+    with Cluster(label="Production/Staging", direction="TB", graph_attr={"id": "cluster_production","bgcolor": "white", "fontsize": "20"}):
 
         with Cluster("Vault"):
             vault = ECS("Vault", id="ec2-vault")
 
-    with Cluster("Kubernetes Cluster", graph_attr={"id": "cluster_k8s"}):
-        with Cluster("Ingress", graph_attr={"id": "cluster_edge_aperture"}):
-            apertureNLB = NLB("Aperture NLB", id="cluster_edge_apertureNLB")
-            traefik = AutoScaling("Aperture Traefik",
-                                id="cluster_edge_traefik")
-            k8sIngress = NLB("Traefik Ingress")
+        with Cluster("Stateful Services", graph_attr={"id": "cluster_data_storage", "fontsize": "20"}):
+            rds = Aurora("Aurora 5.7")
+            dynamo = Dynamodb("DynamoDB")
+            msk = ManagedStreamingForKafka("MSK")
+            datastore = [rds, msk, dynamo]
 
-        servicePods = [Pod("Dashboard PHP", id="dashboard"), Pod("Amplify", id="amplify"), Pod("Owly", id="owlyPod"), Pod("Auth Facade", id="authfacade"), Pod("Webhook Egress Proxy", id="webhookEgressProxy")]
-        servicePod = Pod("Other Microservices Pods... ", id="servicePods")
+    with Cluster("Kubernetes Cluster", graph_attr={"id": "cluster_k8s", "fontsize": "20"}):
+        with Cluster("Ingress", graph_attr={"id": "cluster_edge_aperture"}):
+            traefik = Pod("Traefik Aperture",
+                                id="cluster_edge_traefik")
+            k8sIngress = Pod("Traefik K8s Ingress")
+
+        servicePods = [Pod("Dashboard PHP", id="dashboard"), Pod("Amplify", id="amplify"), Pod("Owly Web", id="owlyPod"), Pod("Auth Facade", id="authfacade"), Pod("Webhook Egress Proxy", id="webhookEgressProxy")]
+        servicePod = Pod("200+ Microservices", id="servicePods")
+
+
 
 # Path:
-    apertureNLB >> Edge(reverse=True, id="cluster_edge") >> traefik >> k8sIngress
-    traefik >> Edge(reverse=True, id="edge_ec2_traefik") >> EC2Services
-    servicePod >> Edge(reverse=True) << msk
     vault >> Edge(reverse=True) << servicePods
-    k8sIngress >> Edge(reverse=True) << servicePods, servicePod
+    servicePod >> Edge(reverse=True, id="edge_datastore") << datastore
+    k8sIngress >> Edge(reverse=True) << servicePods
+    k8sIngress >> Edge(reverse=True) << servicePod
+    traefik >> Edge(reverse=True) << servicePods
+    traefik >> Edge(reverse=True) << servicePod
     servicePod >> Edge(reverse=True, id="edge_datastore") << datastore
